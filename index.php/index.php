0390c34c353e32cf0885455e1cdc8e06e47c35f8
thuhien695 <lethuhien695@gmail.com>
2025-08-16 23:45:19 +0700

    Add index.php

---
+++ index.php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charsrset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Xin chao TMU";
+    ?>
+</body>
+</html>
